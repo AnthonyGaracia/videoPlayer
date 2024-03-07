55bc7286054c97df253f6e0d2b98e91151011ef9
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2024-03-07 12:16:12 -0800

    videoPlaya wrking

--- App.jsx
+++ App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
-import video from './toneDefa.mp4'
+import './App.css';
+import VideoJs from './VideoJs';
+import video from './toneDefa.mp4';
 
 
 
-function App() {
+function App () {
   const videoJsOptions = {
     controls: true,
     sources: [{
       src: video,
-      type: 'videp/mp4'
+      type: 'video/mp4'
     }]
   }
   
 
   return (
    <div>
+    <h1>videoPlayer</h1>
+    <VideoJs options={videoJsOptions} />
 
    </div>
-  )
+  );
 }
 
 export default App
